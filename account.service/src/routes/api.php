9c0bbdba9df4f68f94dabc8508e3351148eefdb2
stark <stark1_5@live.com>
2022-09-02 18:44:57 +0200

    Accounts start

--- account.service/src/routes/api.php
+++ account.service/src/routes/api.php
@@ -18,6 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
