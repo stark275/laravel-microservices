e0e6de345153b7cba69f0cc3025887642100b259
stark <stark1_5@live.com>
2022-09-04 22:25:43 +0200

    services boot scripts

---
+++ boot.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/bash
+
+function firstBoot() {
+
+    docker-compose run --rm composer install
+    chmod -R 777 bootstrap/
+    chmod -R 777 storage/
+
+    echo "lol"
+}
+
+
+ROOT=$(pwd)
+echo $ROOT
+
+SERVICES=$(ls | grep ".service")
+SRC="/src"
+
+for SERVICE in $SERVICES 
+  do
+
+    cd "$SERVICE$SRC"
+    # -------- First boot ----------
+    # firstBoot
+    
+    cd "$ROOT/$SERVICE"
+    # docker-compose up -d --build
+
+    break
+done
+
+
---
+++ down.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+
+
+ROOT=$(pwd)
+echo $ROOT
+
+SERVICES=$(ls | grep ".service")
+SRC="/src"
+
+for SERVICE in $SERVICES 
+  do
+
+    cd "$ROOT/$SERVICE"
+    docker-compose down
+
+    cd $ROOT
+    break
+done
