bad134d16616480e8d44d0a64fa9e41d5ea1e2f4
stark <stark1_5@live.com>
2022-09-05 04:13:56 +0200

    exec script

---
+++ exec.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/bash
+ROOT=$(pwd)
+
+SERVICES=$(ls | grep ".service")
+SRC="/src"
+
+
+read -p "Enter your command: " CMD
+
+for SERVICE in $SERVICES 
+  do
+
+    cd "$SERVICE$SRC"
+    
+    eval $CMD
+
+    cd $ROOT
+done
+
+
