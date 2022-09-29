c3289a953e08d2d7859a13fa78104211f2e14582
stark <stark1_5@live.com>
2022-09-29 21:32:23 +0200

    CI/CD 2

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,27 +5,27 @@ echo "Deployment started ..."
 
 # Enter maintenance mode or return true
 # if already is in maintenance mode
-(php artisan down) || true
+# (php artisan down) || true
 
 # Pull the latest version of the app
-git pull origin production
+git pull origin main
 
 # Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+# composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
-# Clear the old cache
-php artisan clear-compiled
+# # Clear the old cache
+# php artisan clear-compiled
 
-# Recreate cache
-php artisan optimize
+# # Recreate cache
+# php artisan optimize
 
-# Compile npm assets
-npm run prod
+# # Compile npm assets
+# npm run prod
 
-# Run database migrations
-php artisan migrate --force
+# # Run database migrations
+# php artisan migrate --force
 
-# Exit maintenance mode
-php artisan up
+# # Exit maintenance mode
+# php artisan up
 
 echo "Deployment finished!"
